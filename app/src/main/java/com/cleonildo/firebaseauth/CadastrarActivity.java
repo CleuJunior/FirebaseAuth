1241907edd8ee98e1c69d29f38b48e5e854a6d82
Cleonildo Jr <cleu.junior@gmail.com>
2022-07-13 23:48:40 -0300

    CadastrarActivity, Intent and Manifest.

---
+++ app/src/main/java/com/cleonildo/firebaseauth/CadastrarActivity.java
@@ -0,0 +1,42 @@
+package com.cleonildo.firebaseauth;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastrarActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edit_text_email, edit_text_senha, edit_text_senha_repetir;
+    private Button button_cadastrar, button_cancelar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadatras);
+
+        this.edit_text_email = findViewById(R.id.edit_text_email);
+        this.edit_text_senha = findViewById(R.id.edit_text_senha);
+        this.edit_text_senha_repetir = findViewById(R.id.edit_text_repetir_senha);
+        this.button_cadastrar = findViewById(R.id.button_cadastrar_usuario);
+        this.button_cancelar = findViewById(R.id.button_cancelar);
+
+        this.button_cadastrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_cadastrar_usuario:
+                cadastrar();
+                break;
+        }
+    }
+
+    private void cadastrar() {
+        Toast.makeText(this, "Fui Clickado", Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/cleonildo/firebaseauth/MainActivity.java
+++ app/src/main/java/com/cleonildo/firebaseauth/MainActivity.java
@@ -2,15 +2,37 @@ package com.cleonildo.firebaseauth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.ktx.Firebase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonLogin, buttonCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.buttonLogin.findViewById(R.id.button_login);
+        this.buttonCadastrar.findViewById(R.id.button_cadastrar);
+
+        this.buttonLogin.setOnClickListener(this);
+        this.buttonCadastrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_login:
+                break;
+            case R.id.button_cadastrar:
+                startActivity(new Intent(this, CadastrarActivity.class));
+                break;
+        }
     }
 }
