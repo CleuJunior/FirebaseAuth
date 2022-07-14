8b3c1222db2d2e9f0e42ec3d35f60656283434f5
Cleonildo Jr <cleu.junior@gmail.com>
2022-07-13 22:36:38 -0300

    Initial Project

---
+++ app/src/main/java/com/cleonildo/firebaseauth/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cleonildo.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.ktx.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
